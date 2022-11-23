b884e3567e27579974cc32e54c2a6e12d68252f6
Philipp Spiess <hello@philippspiess.com>
2022-11-23 23:44:07 +0100

    Fix the most stupidest bug ever

--- lib/pot/router.ex
+++ lib/pot/router.ex
@@ -1,7 +1,7 @@
 defmodule Pot.Router do
   defmacro pot(path, pot_view, action \\ nil, opts \\ []) do
     quote bind_quoted: binding() do
-      Phoenix.Router.get(path, Pot.Plug, action, [])
+      Phoenix.Router.get(path, Pot.Plug, action, alias: false)
     end
   end
 end
